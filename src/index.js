6539093b4987035482b29fc93137b284a78c0725
Dariell Vasquez <dariellv@yahoo.com>
2018-10-12 17:09:54 -0400

    added link from root url page to newpost page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import PostIndex from './components/PostIndex';
 import NewPost from './components/NewPost';
 import reducers from './reducers';
@@ -12,11 +12,13 @@ const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-    <div>
-    <Route path="/" exact component={PostIndex} />
-    <Route path="/posts" exact component={NewPost} />
-    </div>
-    
+      <div>
+        <Switch>
+          <Route path="/posts/new" component={NewPost} />
+          {/* Always use most specific route first because switch will match component to first matching path */}
+          <Route path="/"  component={PostIndex} />
+        </Switch>
+      </div>
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));

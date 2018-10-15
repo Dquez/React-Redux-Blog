1babc2c98054a3008e78e2dbf136f7f8f15a0c49
Dariell Vasquez <dariellv@yahoo.com>
2018-10-15 12:24:09 -0400

    added redux form to refactor react form

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,81 +1,47 @@
 import React, {Component} from "react";
-import { connect } from "react-redux";
-import { submitPost } from "../actions";
-// connect upgrades NewPost component to container which has access to redux store
+import {Field, reduxForm} from "redux-form";
+// reduxForm upgrades NewPost component to container which has access to redux store
 
 class NewPost extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            "title": "",
-            "categories": "",
-            "content": ""
-        }
-        this.onInputChange = this.onInputChange.bind(this);
-        this.onFormSubmit = this.onFormSubmit.bind(this);
-    }
-    onInputChange(event) {
-        const {value, name} = event.target;
 
-        this.setState({
-            [name]: value
-        })
-    }
-    onFormSubmit(event) {
-        event.preventDefault();
-        console.log(this.state);
-        this.props.submitPost({...this.state})
-        this.setState({
-            title: "",
-            categories: "",
-            content: ""
-        })
-    }
+    renderField (field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input 
+                className="form-control"
+                type="text"
+               {...field.input}
+                />
+            </div>
+        )
 
+    }
 
     render(){
         return(
-            <form onSubmit={this.onFormSubmit}>
-                <div className="form-group">
-                    <label htmlFor="title">Title</label>
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        name="title" 
-                        placeholder="Enter a title"
-                        onChange={this.onInputChange}
-                        value={this.state.title}
-                     />
-                    
-                </div>
-                <div className="form-group">
-                <label htmlFor="category">Category</label>
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        name="categories" 
-                        placeholder="Enter a category" 
-                        onChange={this.onInputChange}
-                    />
-                    
-                </div>
-                <div className="form-group">
-                    <label htmlFor="content">Content</label>
-                    <textarea 
-                        className="form-control" 
-                        name="content" 
-                        rows="3"
-                        onChange={this.onInputChange}>
-                    </textarea>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+            <form>
+                <Field 
+                    name="title"
+                    label="Title"
+                    component={this.renderField}
+                />
+                <Field 
+                    name="tags"
+                    label="Tags"
+                    component={this.renderField}
+                />
+                <Field 
+                    name="content"
+                    label="Post Content"
+                    component={this.renderField}
+                />
+                {/* <button type="submit" className="btn btn-primary">Submit</button> */}
             </form>
         )
     }
 }
 
-
-
-
-// adding destructured method as a prop on NewPost component
-export default connect(null, {submitPost})(NewPost);
+export default reduxForm ({
+    form: 'NewPostForm'
+})(NewPost)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
+import {reducer as formReducer} from 'redux-form';
 
 // link all reducers to a single root reducer, to create the redux store
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
